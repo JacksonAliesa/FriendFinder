8eab427d904324586ddb16841179791bfbe7e0bc
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-11-22 22:27:41 -0500

    fixed error within the $.post section

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,35 +5,37 @@ var friendsArray = [
         "name": "Damian Lillard",
         "photo": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6606.png&w=350&h=254",
         "scores": [
-            2, 1, 5, 3, 7, 1, 1, 5, 4, 5
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1
         ]
     },
     {
         "name": "Lebron James",
         "photo": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png",
         "scores": [
-            5, 4, 5, 3, 2, 1, 1, 3, 3, 5
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2
         ]
     },
     {
         "name": "Stephen Curry",
         "photo": "https://www.mercurynews.com/wp-content/uploads/2019/10/BNG-L-WARRIORS-1011-18.jpg",
         "scores": [
-            5, 5, 5, 3, 2, 1, 1, 1, 1, 1
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+
+
         ]
     },
     {
         "name": "Dwayne Wade",
         "photo": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1987.png",
         "scores": [
-            4, 2, 1, 1, 1, 1, 1, 1, 4, 3
+            4, 4, 4, 4, 4, 4, 4, 4, 4, 4
         ]
     },
     {
         "name": "Kevin Durant",
         "photo": "https://images2.minutemediacdn.com/image/upload/c_fill,w_688,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/Brooklyn-Nets-Media-Day-949a71f35f9f9f6802a81a616e51fc53.jpg",
         "scores": [
-            2, 3, 3, 3, 5, 1, 1, 5, 5, 5
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,7 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on friends
 // ===============================================================================
-
+//this var is for the friends.js file that has the friends array
 var friendData = require("../data/friends");
 
 
@@ -43,23 +43,33 @@ module.exports = function(app) {
 
  //app.post to get the information from the submit button 
 //post route /api/friends used to handle incoming survey results. this route also handles the compatibility logic.
-    // loop over friends array
-    // for(var i = 0; i < friendsArray.length; i++){
-    //   var sum = 0;
-    //   // inside that loop, loop over friends[i].surveyResponsesArray
-    //   for(var j = 0; j < friends[i].surveyResults; j++){
-    //      //compare each survey response with the survey response of the user
-    //      sum += Math.abs(friendsArray[i].score - surveyResults[j])
+    
+    //this gets replaced after we find the difference of the 2 scores only IF it is lower
+    var bestMatch = 50;
+    //closest score will be replaced with the best match score 
+    var bestMatchIndex = 0;
+    
+// loop over friends array
+    for(var i = 0; i < friendData.length; i++){
+      console.log("Friends:" + [i]);
+      console.log("Survey Score:" + friendData[i].score);
+      console.log("New Friends Score:" + [i]);
 
-    //   }
-    // console.log(sum);
-    // }
+      // inside that loop, loop over friends[i].surveyResponsesArray
+      for(var j = 0; j < userResults.surveyResults.length; j++){
+         //compare each survey response with the survey response of the user
+        difference += Math.abs(friendData[i].scores[j] - userResults.surveyResults[j])
+        
+      }
+    console.log(difference);
+    }
     //   //keep a running total
+    //push to friends array
+    friendData.push(req.body);
     //   //need to know who has less of a difference from the friends array VS the user input - (from the friend array)the closest match will be the one 
     //   //with least amt of difference
-    //   var bestMatchScore = friendsArray[i].score;
-    //   var bestMatchIndex = 500; // repersent which friend it is ...the i from the friends array
-
+    // repersent which friend it is ...the i from the friends array
+     
       //another if this bestmatchscore < bestmatchindex reassign the friend
     });
     //end of app-post function
