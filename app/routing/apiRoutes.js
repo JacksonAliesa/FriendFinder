a9701eb8aea1e22042303db7957f932be575a1be
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-11-24 15:13:48 -0500

    found the differences succesfully for the friends array vs user score

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -38,31 +38,50 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
- 
-      console.log(req.body);
+     console.log("Hey Route")
+      console.log("From front-end" +req.body);
+      console.log("Scores" + req.body.scores)
+    
 
  //app.post to get the information from the submit button 
 //post route /api/friends used to handle incoming survey results. this route also handles the compatibility logic.
     
     //this gets replaced after we find the difference of the 2 scores only IF it is lower
-    var bestMatch = 50;
+    var bestMatch = Infinity;
     //closest score will be replaced with the best match score 
-    var bestMatchIndex = 0;
+    var difference = 0;
+
+    var userScore = req.body;
+
+  
+
     
 // loop over friends array
     for(var i = 0; i < friendData.length; i++){
-      console.log("Friends:" + [i]);
-      console.log("Survey Score:" + friendData[i].score);
-      console.log("New Friends Score:" + [i]);
-
+      console.log("Friends:" );
+      //shows friend within the array
+      console.log(friendData[i])
+      console.log("Shows the friend's array Score:" + friendData[i].scores);
+  
       // inside that loop, loop over friends[i].surveyResponsesArray
-      for(var j = 0; j < userResults.surveyResults.length; j++){
+      for(var j = 0; j < userScore.scores.length; j++){
+        console.log("The user Survey score: ")
+        console.log(userScore);
          //compare each survey response with the survey response of the user
-        difference += Math.abs(friendData[i].scores[j] - userResults.surveyResults[j])
+         difference += Math.abs(parseInt(userScore.scores[j]) - friendData[i].scores[j]);
+        };
+        console.log("this is the difference:")
+        console.log(difference);
+
         
-      }
-    console.log(difference);
+        if(difference < bestMatch){
+         bestFriend = friendData[i];
     }
+
+
+}
+//end of first for loop
+  
     //   //keep a running total
     //push to friends array
     friendData.push(req.body);
