5805b7edbfcb9b2499109f9cba41721d14c67308
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-11-24 20:13:38 -0500

    changed friends array scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,21 +5,21 @@ var friendsArray = [
         "name": "Damian Lillard",
         "photo": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6606.png&w=350&h=254",
         "scores": [
-            1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+            1, 5, 3, 5, 5, 4, 4, 4, 3, 2
         ]
     },
     {
         "name": "Lebron James",
         "photo": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png",
         "scores": [
-            2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+            1, 2, 5, 4, 2, 2, 2, 3, 3, 3
         ]
     },
     {
         "name": "Stephen Curry",
         "photo": "https://www.mercurynews.com/wp-content/uploads/2019/10/BNG-L-WARRIORS-1011-18.jpg",
         "scores": [
-            3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+            4, 5, 5, 5, 4, 3, 3, 3, 3, 3
 
 
         ]
@@ -28,14 +28,14 @@ var friendsArray = [
         "name": "Dwayne Wade",
         "photo": "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1987.png",
         "scores": [
-            4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+            1, 4, 4, 4, 1, 1, 4, 4, 1, 2
         ]
     },
     {
         "name": "Kevin Durant",
         "photo": "https://images2.minutemediacdn.com/image/upload/c_fill,w_688,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/Brooklyn-Nets-Media-Day-949a71f35f9f9f6802a81a616e51fc53.jpg",
         "scores": [
-            5, 5, 5, 5, 5, 5, 5, 5, 5, 5
+            4, 5, 5, 5, 5, 3, 3, 5, 1, 2
         ]
     }
 ];
